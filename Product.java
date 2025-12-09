a91dd8152704e5b6ef4a381df5dffa44f5290ee3
charaouikik <charaouikik@gmail.com>
2025-12-08 21:18:54 -0500

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,17 +1,28 @@
 package ExpenseTracker;
+import java.time.LocalDate;
 // Class for Product Objects
 public class Product {
     private String name;
     private double price;
-    public Product(String name, double price) {
+    private String category;
+    private LocalDate date;
+    public Product(String name,double price,String category, LocalDate date) {
         this.name = name;
         this.price = price;
+        this.category = category;
+        this.date=date;
     }
     public String getName() {
         return name;
     }
     public double getPrice() {
         return price;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public LocalDate getDate() {
+        return date;
     }
     @Override
     public String toString() {
